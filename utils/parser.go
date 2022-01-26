cf7dda6c3a1e16b18b9465583f5489658d05dd53
Aron Lebani <aron@lebani.dev>
2022-01-26 12:52:38 +1100

    Add youtube videos to entries list view

--- utils/parser.go
+++ utils/parser.go
@@ -5,20 +5,51 @@ import (
     "golang.org/x/net/html"
 )
 
-func GetImgTag(htmlString string) string {
+func isImg(tn []byte) bool {
+    return string(tn) == "img"
+}
+
+func isIframe(tn []byte) bool {
+    return string(tn) == "iframe"
+}
+
+func isYoutubeEmbed(tn []byte, z *html.Tokenizer) bool {
+    if !isIframe(tn) {
+        return false
+    }
+
+    for {
+        key, val, moreAttr := z.TagAttr()
+        switch string(key) {
+        case "src":
+            return strings.Contains(string(val), "youtube")
+        default:
+            if moreAttr {
+                continue
+            } else {
+                break
+            }
+        }
+    }
+}
+
+func GetMediaElement(htmlString string) string {
 	r := strings.NewReader(htmlString)
 	z := html.NewTokenizer(r)
 
 	for {
 		tt := z.Next()
+        tn, _ := z.TagName()
+		raw := z.Raw()
 		switch tt {
 		case html.ErrorToken:
 			return ""
 		case html.SelfClosingTagToken, html.StartTagToken:
-			tn, _ := z.TagName()
-			if string(tn) == "img" {
-				raw := z.Raw()
+			if isImg(tn) {
 				return string(raw)
+			}
+			if isYoutubeEmbed(tn, z) {
+				return string(raw) + "</iframe>"
 			}
 		}
 	}
